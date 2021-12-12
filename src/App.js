a16edf827f30093474f825933a86acbaadef56a3
PauloDev77 <paulomadson16@gmail.com>
2021-12-12 05:21:30 -0300

    Services

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Home from './Home.js';
 import About from "./About.js";
+import Service from "./Service.js";
 
 function App() {
   return (
     <div className="App">
         <Home />
         <About/>
-
+       <Service/>
     </div>
   );
 }
