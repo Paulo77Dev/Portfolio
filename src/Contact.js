6ba3a96b1d2ee73b1e1eacbe141375c273f6170c
PauloDev77 <paulomadson16@gmail.com>
2021-12-20 19:08:53 -0300

    Inserindo atualizações do formulario de contato

--- src/Contact.js
+++ src/Contact.js
@@ -1,32 +1,49 @@
 import React from 'react';
-import './Contact.css';
+import './Contact.css';   
+import emailjs from "emailjs-com";
 import contactImg from "./img/about-9.png";
-function Contact() {
-    return (
-        <div className="contact component__space" id="Contact">
+
+export default function Contact() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+    emailjs.sendForm('gmail_paulo', 'template_gwwdfdg', e.target, 'user_WRbuOJOeUKm3u6hk8L9FS')
+
+        .then((result) => {
+            alert("Mensagem enviada com sucesso! 👍");
+           
+        }, (error) => {
+            alert(error.message)
+            
+        });
+        e.target.reset()
+ 
+    }
+    return(
+<div className="contact component__space" id="Contact">
             <div className="row">
                 <div className="col__2">
                     <div className="contact__box">
                         <div className="contact__meta">
                             <h1 className="hire__text">Contrate - Me.</h1>
-                            <p className="hire__text white">Estou disponível para trabalho autônomo. Conecte-se comigo por telefone:</p>
-                           <p className="hire__text white"><strong>+55 53 99947 - 1481</strong> e email <strong>paulomadson16@gmail.com</strong></p>
-                        </div>
-                        <div className="input__box">
-                           <input type="text" className="contact name" placeholder="Seu Nome *" />
-                           <input type="text" className="contact email" placeholder="Seu Email *" />
-                           <input type="text" className="contact subject" placeholder="Escreva um assunto" />
-                           <textarea name="message" id="message" placeholder="Escreva sua mensagem"></textarea>
-                           <button className="btn contact pointer" type="submit">Enviar</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="col__2">
+                                <p className="hire__text white">Estou disponível para trabalho autônomo. Conecte-se comigo por telefone:</p>
+                                    <p className="hire__text white"><strong>+55 53 99947 - 1481</strong> e email <strong>paulomadson16@gmail.com</strong></p>
+                                    </div>                
+                                <div className="input__box">
+                            <form onSubmit={sendEmail}>
+                        <input type="text" autoFocus className="contact name" required placeholder="Nome" name="name"/>
+                        <input type="email" className="contact email" required placeholder="Seu email" name="email"/>
+                        <textarea name="message" id="message" placeholder="Escreva sua mensagem"></textarea>
+                        <input type="submit" className="btn contact pointer" value="Enviar mensagem"></input>
+                            </form>    
+                                </div>
+                                </div>
+                            </div>
+                        <div className="col__2">
                     <img src={contactImg} alt="" className="contact__img" />
                 </div>
             </div>
         </div>
     )
 }
-
-export default Contact
